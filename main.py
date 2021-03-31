6e900b24ca7b268206f09263b5813def1924a83e
Danya Generalov <danya@danya02.ru>
2021-04-01 00:08:05 +0300

    Adding main functionality.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import random
 import datetime
 import hashlib
 import traceback
+import asyncio
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -21,18 +22,21 @@ intents.voice_states = True
 
 bot = commands.Bot(command_prefix=os.getenv('BOT_COMMAND_PREFIX') or '>', intents=intents)
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://mongo:27017/')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://mongo:27017/', username='root', password='rootpassword')
 db = client.db
-rules = db.rules
 
-def generate_name_indexes(attempts = 50):
+async def generate_name_indexes(attempts = 50):
     for _ in range(attempts):
         left_ind = random.randint(0, len(names.LEFT)-1)
         center_ind = random.randint(0, len(names.CENTER)-1)
         right_ind = random.randint(0, len(names.RIGHT)-1)
         indexes = [left_ind, center_ind, right_ind]
-        # TODO: check if indexes are already in db
+        existing = await db.rules.find_one({'name_indexes': indexes})
+        if existing is not None:
+            continue
+
         return indexes
+    raise ValueError('Could not find unclaimed name indexes!')
 
 def name_indexes_to_words(indexes):
     outp = []
@@ -83,6 +87,9 @@ class Userlike:
         else:
             return '<@&'+str(self.id)+'>'
 
+    def to_json(self):
+        return {'type': self.type.value, 'id': self.id}
+
 class Action(enum.Enum):
     JOINS = 'joins'
     LEAVES = 'leaves'
@@ -127,6 +134,11 @@ class Trigger:
             action = Action(action)
         self.action = action
 
+    def to_json(self):
+        return {'userlike': self.userlike.to_json() if self.userlike else None,
+                'action': self.action.value,
+                'channel': self.channel.id if self.channel else None}
+
 class Event:
     def __init__(self, *, user, state_before, state_after):
         self.channel = state_before.channel or state_after.channel
@@ -167,22 +179,63 @@ class Event:
     def __repr__(self):
         return f'Event<user={repr(self.user)}, action={repr(self.action)}, channel={repr(self.channel)}>'
 
+    async def lookup_rules(self):
+        q = []
+        
+        # match user
+        this_user = {'trigger.userlike.type': UserlikeType.MEMBER.value,
+                     'trigger.userlike.id': self.user.id}
+        
+        # match roles of user
+        role_queries = []
+        for role in self.user.roles:
+            role_queries.append({'trigger.userlike.id': role.id})
+
+        these_roles = {'$and':
+                          [
+                            {'trigger.userlike.type': UserlikeType.ROLE.value},
+                            {'$or': role_queries}
+                          ]
+                      }
+
+        # rule does not limit user
+        no_user_limitation = {'trigger.userlike': None}
+
+        correct_user = {'$or': [this_user, these_roles, no_user_limitation]}
+
+        correct_action = {'trigger.action': self.action.value}
+
+        correct_channel = {'$or': [{'trigger.channel': self.channel.id}, {'trigger.channel': None}]}
+        
+        compound_query = {'$and': [{'guild': self.user.guild.id}, correct_user, correct_action, correct_channel]}
+        rules = await db.rules.find(compound_query).to_list(None)
+        rules = rules or []
+        rules = [Rule(**i) for i in rules]
+
+        return rules
+
 class Rule:
-    def __init__(self, *, guild, trigger, channel_to_mention, users_to_mention):
-        self.guild = guild
+    def __init__(self, *, guild, trigger, channel_to_mention, users_to_mention, name_indexes=None, **kwargs):
+        if isinstance(guild, discord.Guild):
+            self.guild = guild
+        else:
+            self.guild = bot.get_guild(guild)
         if not isinstance(trigger, Trigger):
             trigger = Trigger(**trigger)
         self.trigger = trigger
+        users_to_mention = users_to_mention or []
         if not isinstance(channel_to_mention, discord.TextChannel):
-            raise TypeError('channel_to_mention must be a TextChannel')
+            channel_to_mention = self.guild.get_channel(channel_to_mention)
         self.channel_to_mention = channel_to_mention
-        self.users_to_mention = [Userlike(**i) if isinstance(i, dict) else Userlike.from_discord_model(i) if isinstance(i, discord.Role) or isinstance(i, discord.Member) else i for i in users_to_mention]
-        self.name_indexes = generate_name_indexes()
+        self.users_to_mention = [Userlike(**i) if isinstance(i, dict) else Userlike.from_discord_model(i) if isinstance(i, discord.Role) or isinstance(i, discord.Member) else i for i in users_to_mention if i]
+        self.name_indexes = name_indexes
 
     @property
     def name(self):
         return name_indexes_to_words(self.name_indexes)
 
+    async def generate_name(self):
+        self.name_indexes = await generate_name_indexes()
 
     @property
     def name_hash(self):
@@ -213,15 +266,47 @@ class Rule:
             emb.add_field(name='While mentioning these', value=' '.join(map(lambda x: x.as_mention(), self.users_to_mention)))
         return emb
 
-    async def send_notification(self, ctx, event):
+    async def send_notification(self, event):
         notification_text = ' '.join(map(lambda x: x.as_mention(), self.users_to_mention))
         notification_text += ' ' + ACTION_MESSAGES[self.trigger.action].format(user=event.user, channel=event.channel)
         emb = discord.Embed()
         emb.color = self.color
         emb.description = 'This notification was created by rule `'+self.name+'`.'
         emb.timestamp = datetime.datetime.now()
-        await ctx.send(content=notification_text, embed=emb)
+        await self.channel_to_mention.send(content=notification_text, embed=emb)
+
+    @staticmethod
+    async def send_notifications_for_list(event, rules):
+        notification_text = ACTION_MESSAGES[rules[0].trigger.action].format(user=event.user, channel=event.channel)
+        mentions = dict()
+        contributing_rules = dict()
+        for rule in rules:
+            mentions[rule.channel_to_mention] = mentions.get(rule.channel_to_mention, []) + (rule.users_to_mention or [])
+            contributing_rules[rule.channel_to_mention] = contributing_rules.get(rule.channel_to_mention, []) + [rule]
         
+        for channel in contributing_rules:
+            emb = discord.Embed()
+            rules_for_channel = contributing_rules[channel]
+            emb.timestamp = datetime.datetime.now()
+            if len(rules_for_channel)>1:
+                emb.color = discord.Color.random() # because multiple rules are being applied, no one color may be used.
+                emb.description = 'This notification was created by these rules: `' + '`, `'.join([i.name for i in rules_for_channel])+'`.'
+            else:
+                emb.color = rules_for_channel[0].color
+                emb.description = 'This notification was created by rule `'+rules_for_channel[0].name+'`.' 
+            mentions_for_channel = ' '.join([i.as_mention() for i in mentions[channel]])
+            try:
+                await channel.send(content=mentions_for_channel + ' ' + notification_text, embed=emb)
+            except discord.Forbidden: pass
+            
+        
+    def to_json(self):
+        return {'guild': self.guild.id,
+                'trigger': self.trigger.to_json(),
+                'channel_to_mention': self.channel_to_mention.id,
+                'users_to_mention': [i.to_json() for i in self.users_to_mention],
+                'name_indexes': self.name_indexes}
+
 
 
 @bot.command()
@@ -251,19 +336,76 @@ async def add_rule(ctx,
             await ctx.send('Action `'+does_what+'` is not recognized, valid options are: `'+'`, `'.join(map(lambda x: x.value, Action))+'`.')
         trig = Trigger(userlike=who, action=does_what, channel=in_where)
         rule = Rule(guild=ctx.channel.guild, trigger=trig, channel_to_mention=ctx.channel, users_to_mention=tell_who)
-        await ctx.send('Your new rule is:', embed=rule.as_embed())
+        chk = rule.to_json()
+        del chk['name_indexes']
+        existing = await db.rules.find_one(chk)
+        if existing:
+            rule = Rule(**existing)
+            await ctx.send(content='A rule that is identical to this one already exists, not registering.', embed=rule.as_embed())
+            return
+
+        await rule.generate_name()
+        member_is_manager = ctx.author.guild_permissions.manage_guild
+        if not member_is_manager:
+            tell_who = tell_who or [ctx.author]
+            if len(tell_who)>1 or tell_who[0] != ctx.author:
+                ctx.send(ctx.author.mention+''', you have tried to mention people other than yourself with this rule without having the "Manage Server" permission.
+Please edit your rule to exclude other users from the list of people to be notified.''', embed=rule.as_embed())
+                return
+        CHECK_MARK = '✅'
+        CANCEL_MARK = '❌'
+        emojis = [CHECK_MARK, CANCEL_MARK]
+        msg = await ctx.send('Here is your new rule. Please confirm the parameters, then click '+CHECK_MARK+' to confirm or '+CANCEL_MARK+' to cancel.', embed=rule.as_embed())
+        try:
+            await msg.add_reaction(CHECK_MARK)
+            await msg.add_reaction(CANCEL_MARK)
+        except discord.Forbidden:
+            await msg.edit(content='This bot cannot add reactions to messages, but this is required.')
+            await cannot_add_reactions(ctx)
+            return
+        try:
+            reaction, _ = await bot.wait_for('reaction_add',
+                                             check=lambda r, u: r.emoji in emojis and r.message.id == msg.id and u == ctx.author,
+                                             timeout=120)
+        except asyncio.TimeoutError:
+            await msg.edit(content='Rule confirmation timed out, to confirm this rule please repeat the command.')
+            try:
+                await msg.clear_reactions()
+            except discord.Forbidden:
+                await cannot_clear_reactions(ctx)
+        reaction = reaction.emoji
+        if reaction == CANCEL_MARK:
+            await msg.edit(content='Rule cancelled, this rule will not be applied.')
+            try:
+                await msg.clear_reactions()
+            except discord.Forbidden:
+                await cannot_clear_reactions(ctx)
+        else:
+            await db.rules.insert_one(rule.to_json())
+            await msg.edit(content='Rule confirmed.')
+            try:
+                await msg.clear_reactions()
+            except discord.Forbidden:
+                await cannot_clear_reactions(ctx)
     except Exception as e:
         await on_command_error(ctx, e)
 
 @bot.event
 async def on_voice_state_update(member, before, after):
     try:
-#        try:
-        ev = Event(user=member, state_before=before, state_after=after)
-#        except ValueError: # if no handleable change occurred, ignore.
-#            return
-    
-        await member.send('You just caused this event: '+repr(ev))
+        try:
+            ev = Event(user=member, state_before=before, state_after=after)
+        except ValueError: # if no handleable change occurred, ignore.
+            return
+
+        rules = await ev.lookup_rules()
+        if rules:
+            if len(rules)==1:
+                await rules[0].send_notification(ev)
+            else:
+                await Rule.send_notifications_for_list(ev, rules)
+
+        #await member.send('You just caused this event: '+repr(ev))
     except Exception as e:
         await on_command_error(None, e, guild=member.guild)
 
@@ -303,5 +445,11 @@ async def on_command_error(ctx, exception, guild=None):
         
 
     return False
+
+async def cannot_add_reactions(ctx):
+    await ctx.send('This bot cannot add reactions to messages. Please allow this bot the "Add Reactions" permission.')
+
+async def cannot_clear_reactions(ctx):
+    await ctx.send('This bot cannot remove reactions on messages. Please allow this bot the "Manage Messages" permission.')
 
 bot.run(TOKEN)
